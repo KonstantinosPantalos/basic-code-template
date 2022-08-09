880f9009f4bb12b0dd84584c7205fb04bd5f65a6
Konstantinos Pantalos <konstantinospantalos@MacBook-Pro-Konstantinos.local>
2022-08-09 12:24:14 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+// $(document).ready(function () {
+//   $(".full-landing-image").ripples({
+//     resolution: 500,
+//     perturbance: 0.03,
+//     dropRadius: 20,
+//     interactive: true,
+//   });
+// });
